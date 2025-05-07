ec1aac4d611e19f489eccd5260c0e95b2890ef51
이예린 <yerin@iyelin-ui-MacBookPro.local>
2025-05-07 10:42:35 +0900

    init

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,6 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
+const { mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +8,27 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const baseConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const config = {
+  resolver: {
+    ...baseConfig.resolver,
+    sourceExts: [...baseConfig.resolver.sourceExts, 'mjs'],
+    resolveRequest: (context, moduleName, platform) => {
+      if (moduleName === 'styled-components') {
+        return {
+          type: 'sourceFile',
+          filePath: path.resolve(
+            __dirname,
+            'node_modules/styled-components/native/dist/styled-components.native.cjs.js'
+          ),
+        };
+      }
+      return context.resolveRequest
+        ? context.resolveRequest(context, moduleName, platform)
+        : require('metro-resolver').resolve(context, moduleName, platform);
+    },
+  },
+};
+
+module.exports = mergeConfig(baseConfig, config);
